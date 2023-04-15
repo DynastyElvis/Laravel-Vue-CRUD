97ca3f37a681d674dbdcdea166257309e9602c46
kipkemoilvs <kipkemoilvs@gmail.com>
2023-04-15 12:08:55 +0300

    repository logic app fixed

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -1,4 +1,39 @@
+<?php
 
+namespace App\Repositories;
+
+interface BaseRepository
+{
+    /**
+     * Get all
+     *
+     * @return mixed
+     */
+    function getAll();
+
+    /**
+     * Get by id
+     *
+     * @param int $id
+     * @return mixed
+     */
+    function getById(int $id);
+
+    /**
+     * Create new
+     *
+     * @param array $attributes
+     * @return mixed
+     */
+    function create(array $attributes);
+
+    /**
+     * Update
+     *
+     * @param int $id
+     * @param array $attributes
+     * @return mixed
+     */
     function update(int $id, array $attributes);
 
     /**
