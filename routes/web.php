f8c6b4f4b7e46a73dc0bd5390632170c63d60772
kipkemoilvs <kipkemoilvs@gmail.com>
2023-04-15 12:07:33 +0300

    ft: routes

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::resource('items', 'ItemController');
+
+Route::get('/{q1?}/{q2?}/{q3?}', function () {
+    return view('welcome');
+});
